57a841f531b236490007b81780ab8a90f757ab8e
Christof Torres <freerider_1991@hotmail.com>
2013-04-29 22:39:04 +0200

    WarenkorbArtikel

---
+++ src/shop/local/valueobjects/WarenkorbArtikel.java
@@ -0,0 +1,47 @@
+package shop.local.valueobjects;
+
+/**
+ * Klasse zur Repräsentation einzelner Warenkorb Artikel.
+ * 
+ * @author Christof Ferreira Torres
+ */
+public class WarenkorbArtikel {
+
+	// Attribute zur Beschreibung eines Warenkorb Artikels:
+	private Artikel artikel;
+	private int stueckzahl;
+	
+	WarenkorbArtikel(Artikel artikel, int stueckzahl) {
+		this.artikel = artikel;
+		this.stueckzahl = stueckzahl;
+	}
+	
+	public void setArtikel(Artikel artikel) {
+		this.artikel = artikel;
+	}
+	
+	public Artikel getArtikel() {
+		return artikel;
+	}
+	
+	public void setStueckzahl(int stueckzahl) {
+		this.stueckzahl = stueckzahl;
+	}
+	
+	public int getStueckzahl() {
+		return stueckzahl;
+	}
+	
+	public String toString() {
+		return ("Nr: " + artikel.getArtikelnummer() + "\t Bezeichnung: " + artikel.getBezeichnung() + "\t Stückzahl: " + stueckzahl);
+	}
+	
+	public boolean equals(Object andererWarenkorbArtikel) {
+		if (andererWarenkorbArtikel instanceof WarenkorbArtikel) {
+			return (this.artikel.equals(((WarenkorbArtikel) andererWarenkorbArtikel).getArtikel()));
+		} else {
+			return false;
+		}
+	}
+	
+}
